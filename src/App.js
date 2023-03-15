14a0892e068cc2dd25497dc3ff24e51b69ea57c6
VimalU1437 <vimalhbk476@gmail.com>
2023-03-15 19:16:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state,setState] = useState([]);
+
+
+  useEffect(()=>{
+    fCall();
+  },[]);
+
+  async function fCall(){
+    const res = await fetch("https://dummyjson.com/users?limit=10");
+    const data = await res.json();
+    setState(()=>{
+      let temp = [...data.users];
+      temp.forEach(elm=>{
+        elm.toggled = false;
+      })
+      // console.log(temp);
+      return temp;
+    });
+    // console.log(data.users);
+  }
+  function togClick(id){
+    setState(pre=>{
+      let temp = [...pre];
+      temp.forEach(elm=>{
+        if(elm.id === id){
+          console.log(elm);
+          elm.toggled = true;
+          return;
+        }
+      })
+      return temp;
+    })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.length !== 0 && state.map(elm=>{
+        return <div className='container' key={elm.id} >
+          <img className='pro-img' src={elm.image} alt={`${elm.firstName}'s pic`} />
+          <div className='info-div'>
+            <p>Name : {`${elm.firstName} ${elm.lastName}`}</p>
+            <p>Email : {elm.email}</p>
+            {
+              elm.toggled ? <p>{elm.age}</p> :<button onClick={()=>togClick(elm.id)}>Toggle age</button> 
+            }
+          </div>
+        </div>
+      })}
+
     </div>
   );
 }
